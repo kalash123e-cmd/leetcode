44bc5b137a8d39a58cee995a1cef5fda996ea059
Kalash Hayaran <kalash123e@gmail.com>
2026-06-25 15:07:34 +0530

    Time: 2 ms (15.18%) | Memory: 8.8 MB (89.03%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0; i<s.size(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+                continue;
+            }
+            if(st.empty() && (s[i] == ')' || s[i] == '}' || s[i] == ']')){
+                return false;
+            }
+            if(s[i] == ')'){
+                s[i] = '(';
+            }
+            if(s[i] == '}'){
+                s[i] = '{';
+            }
+            if(s[i] == ']'){
+                s[i] = '[';
+            }
+            if(s[i] == st.top()){
+                st.pop();
+            }
+            else{
+            return false;
+        }
+        }
+        if(st.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
