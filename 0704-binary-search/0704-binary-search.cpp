d0eea779c5035a997da626e269d19e5d16a48ac2
Kalash Hayaran <kalash123e@gmail.com>
2026-07-02 14:14:09 +0530

    Time: 33 ms (0.62%), Space: 31.3 MB (81.3%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int t) {
+        int i = 0;
+        int j = arr.size()-1;
+        while(i<=j){
+            int mid = (j + i) / 2;
+            if(arr[mid] == t){
+                return mid ;
+            }
+            if(arr[mid]<t){
+                i = mid +1;
+                // continue;
+            }
+            if(arr[mid]>t){
+                j = mid -1;
+            }
+        }
+        return -1;
+    }
+};
