71cab589f20d10e72cb2422fe1b3f7732b90691f
Kalash Hayaran <kalash123e@gmail.com>
2026-07-04 00:36:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (38.28%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long divide(long long a, long long b) {
+        if (a == -2147483648LL && b == -1) {
+            return 2147483647;
+        }
+        return a/b;
+    }
+};
