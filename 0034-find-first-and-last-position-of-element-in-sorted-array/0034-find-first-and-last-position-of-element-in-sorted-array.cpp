4b9939983b415d4ba3f02a4c8413f9539efddedf
Kalash Hayaran <kalash123e@gmail.com>
2026-07-03 13:56:00 +0530

    Time: 0 ms (100%), Space: 17.4 MB (89.31%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int target) {
-        // int l=-1;
-        // int u=-1;
-        int l = lower_bound(arr.begin(),arr.end(),target) - arr.begin();
-        int u = upper_bound(arr.begin(),arr.end(),target) - arr.begin();
-        
-        if(l == arr.size() || arr[l]!= target){
+    vector<int> searchRange(vector<int>& arr, int t) {
+        int l = -1;
+        int u = -1;
+        int i = 0; 
+        int j = arr.size() - 1;
+        if(arr.size() == 0){
             return {-1,-1};
         }
-        return {l,u-1};
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(arr[mid] < t){
+                i = mid+1;
+            }
+            else{
+                l = mid;
+                j = mid-1;
+            }
+        }
+        if(l == -1 || arr[l] != t){
+            return {-1,-1};
+        }
+        i = 0;
+        j = arr.size() - 1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(arr[mid] > t){
+                j = mid-1;
+            }
+            else{
+                u = mid;
+                i = mid+1;
+            }
+        }
+        // if(arr[l] != t || arr[u] != t || l == -1 || u == -1){
+        //     return {-1,-1};
+        // }
 
+        return {l,u};
         
     }
 };
