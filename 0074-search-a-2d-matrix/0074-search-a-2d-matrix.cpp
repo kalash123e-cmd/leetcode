0fe530f917d916825273f50733ac47f9fc7bd8d0
Kalash Hayaran <kalash123e@gmail.com>
2026-07-05 22:36:45 +0530

    Time: 0 ms (100%), Space: 13.4 MB (7.39%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        for(int i = 0; i<arr.size(); i++){
+            for(int j = 0; j<arr[0].size(); j++){
+                if(arr[i][j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
