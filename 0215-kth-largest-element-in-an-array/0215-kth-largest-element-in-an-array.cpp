deded8d186ef1ec6401e1d71e194f7f6f69cea1a
Kalash Hayaran <kalash123e@gmail.com>
2026-07-09 01:46:02 +0530

    Time: 32 ms (61.27%), Space: 72.5 MB (37.26%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,23 +4,18 @@ public:
         priority_queue<int,vector<int>,greater<int>> pq;
         // priority_queue<int> pq;
         // int count = k;
-        for(int i = 0; i<arr.size(); i++){
-            if(pq.size() < k){
+        for(int i = 0; i<k; i++){
             pq.push(arr[i]);
-            continue;
-            }
-            pq.push(arr[i]);
-            pq.pop();
         }
         // for(int i = 0; i<arr.size(); i++){
             
         // }
-        // for(int i = k; i<arr.size(); i++){
-        //     if(arr[i] > pq.top()){
-                // pq.pop();
-                // pq.push(arr[i]);
-        //     }
-        // }
+        for(int i = k; i<arr.size(); i++){
+            if(arr[i] > pq.top()){
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
         return pq.top();
     }
 };
