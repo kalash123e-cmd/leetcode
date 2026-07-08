62b4496fa967b6c10c93b58f7230e27731ed77eb
Kalash Hayaran <kalash123e@gmail.com>
2026-07-09 01:14:13 +0530

    Time: 2 ms (78.66%), Space: 7.9 MB (37.93%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 ){
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int fibo ;
+        for(int i = 2; i<=n; i++){
+            fibo = a+b;
+            a = b;
+            b = fibo;
+        }
+        return fibo;
+    }
+};
