4cc6b0b0538f484bfde291ca5c7babf007d5d70c
Kalash Hayaran <kalash123e@gmail.com>
2026-07-09 02:16:04 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.77%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& arr) {
+        int sum = 0;
+        int no = 0;
+        for(int i = 0; i<arr.size(); i++){
+            sum = sum+arr[i];
+            // no = no+(i-1);
+        }
+        for(int i = 0; i<=arr.size(); i++){
+            no = no+i;
+        }
+        return no - sum;
+    }
+};
