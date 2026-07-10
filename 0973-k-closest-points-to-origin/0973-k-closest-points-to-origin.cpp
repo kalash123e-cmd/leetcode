b757adb6e93e93cf9817648d2f7f128077bed4b9
Kalash Hayaran <kalash123e@gmail.com>
2026-07-11 00:46:50 +0530

    Time: 58 ms (50.09%), Space: 77 MB (41.15%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& arr, int k) {
+        priority_queue<pair<int,vector<int>>> pq;
+        for(int i = 0; i<k; i++){
+            int dist = (arr[i][0]*arr[i][0]) + (arr[i][1]*arr[i][1]);
+            pq.push({dist,arr[i]});
+        } 
+        for(int i = k; i<arr.size(); i++){
+            int dist = (arr[i][0]*arr[i][0]) + (arr[i][1]*arr[i][1]);
+            if(dist > pq.top().first){
+                continue;
+            }
+            pq.push({dist,arr[i]});
+            pq.pop();
+        }
+        vector<vector<int>> res;
+        while(!pq.empty()){
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
