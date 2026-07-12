f158679c747244a11417230f2a818607faf4c1bf
Kalash Hayaran <kalash123e@gmail.com>
2026-07-13 00:04:40 +0530

    Time: 39 ms (55.31%), Space: 46.3 MB (16.28%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> temp;
+        vector<int> t;
+        vector<int> res;
+        for(int i = 0; i<arr.size(); i++){
+            temp.push_back(arr[i]);
+        }
+        sort(temp.begin(),temp.end());
+
+        unordered_map<int,int> f;
+        int rank = 1;
+        for(int i = 0; i<arr.size(); i++){
+            if(f.find(temp[i]) == f.end()){
+                f[temp[i]] = rank;
+                rank++;
+            }
+        }
+        for(int i = 0; i<arr.size(); i++){
+            res.push_back(f[arr[i]]);
+        }
+        return res;
+    }
+};
