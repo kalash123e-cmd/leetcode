65d219834b313b6ae9e8bf499295567e169e1b1b
Kalash Hayaran <kalash123e@gmail.com>
2026-07-13 19:27:16 +0530

    Time: 3 ms (27.77%), Space: 12.6 MB (25.66%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+void fun(vector<int>& arr,int n,int i, vector<int>& temp, vector<vector<int>>& res){
+            if(i == n){
+                res.push_back(temp);
+                return;
+            }
+            fun(arr, n, i+1, temp, res);
+            temp.push_back(arr[i]);
+            fun(arr, n, i+1, temp, res);
+            temp.pop_back();
+}
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<int> temp;
+        vector<vector<int>> res;
+        // vector<vector<int>> res;
+        int n = arr.size();
+        // int i = 0;
+        fun(arr,n,0,temp,res);
+        return res;
+        
+    }
+};
