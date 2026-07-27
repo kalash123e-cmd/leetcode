46d8721d9731d1b61091ae5a2ec1bf00d9e039a9
Kalash Hayaran <kalash123e@gmail.com>
2026-07-27 10:53:15 +0530

    Time: 0 ms (100%), Space: 23.2 MB (21.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,25 +6,26 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    int fun(TreeNode* root, bool &ans){
-        if(root == NULL){
+    int fun(TreeNode* root, bool& ans) {
+        if (root == NULL) {
             return 0;
         }
-        int left =fun(root->left,ans);
-        int right =fun(root->right,ans);
-        if(abs(left - right) >1){
-            ans =  false;
+        int left = fun(root->left, ans);
+        int right = fun(root->right, ans);
+        if (abs(left - right) > 1) {
+            ans = false;
         }
-        return 1+ max(left,right);
+        return 1 + max(left, right);
     }
     bool isBalanced(TreeNode* root) {
         bool ans = true;
-         fun(root,ans);
-         return ans;
+        fun(root, ans);
+        return ans;
     }
 };
