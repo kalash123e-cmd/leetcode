6919f25a4681d616bdfa761b54933bf02ffa4bae
Kalash Hayaran <kalash123e@gmail.com>
2026-08-15 01:18:29 +0530

    Time: 4 ms (3.53%), Space: 9.9 MB (5.02%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res;
+        stack<int> st;
+        for(int i = 0; i<s.size(); i++){
+            if(!(isdigit(s[i]))){
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
+            }
+        }
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
