c3057e3a3c97b032f89d9f055952399e6e99fbbf
Kalash Hayaran <kalash123e@gmail.com>
2026-08-16 19:37:58 +0530

    Time: 0 ms (100%), Space: 22 MB (45.41%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    void fun(TreeNode* root,vector<int> &temp){
+    bool fun(TreeNode* root,vector<int> &temp, long long &prev){
         if(root == NULL){
-            return;
+            return true;
         }
-        fun(root->left,temp);
-        temp.push_back(root->val);
-        fun(root->right,temp);
+        bool r1 =  fun(root->left,temp,prev);
+        // temp.push_back(root->val);
+        if(prev == 1000000000){
+            prev = root->val;
+        }
+        else{
+            if(prev >= root->val){
+                return false;
+            }
+            prev = root->val;
+        }
+        bool r2 =  fun(root->right,temp,prev);
+        return r1 && r2;
+        
     }
     bool isValidBST(TreeNode* root) {
         vector<int> temp;
-        fun(root,temp);
-        for(int i = 0; i<temp.size()-1; i++){
-            if(temp[i] >= temp[i+1]){
-                return false;
-            }
-        }
-        return true;
+        long long prev = 1000000000;
+        return fun(root,temp,prev);
+        // for(int i = 0; i<temp.size()-1; i++){
+        //     if(temp[i] >= temp[i+1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
 
     }
 };
