69ce888735a1e15510bad7fc677b2ca12c00ebc3
Kalash Hayaran <kalash123e@gmail.com>
2026-08-16 20:08:43 +0530

    Time: 1 ms (7.43%), Space: 22 MB (16.09%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -5,20 +5,19 @@ public:
             return true;
         }
         bool r1 = fun(root->left, prev);
-        if (prev == -9223372036854775808) {
-            prev = root->val;
-        } 
-        else {
+        // if (prev == -9223372036854775808) {
+        //     prev = root->val;
+        // } 
+        // else {
             if (prev >= root->val) {
                 return false;
             }
             prev = root->val;
-        }
+        // }
         bool r2 = fun(root->right, prev);
         return r1 && r2;
     }
     bool isValidBST(TreeNode* root) {
-        // vector<int> temp;
         long long prev = -9223372036854775808;
         return fun(root, prev);
     }
