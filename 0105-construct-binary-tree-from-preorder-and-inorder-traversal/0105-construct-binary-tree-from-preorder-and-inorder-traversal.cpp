d5fcedc01e96564d065ba9561edd7d7a7609f553
Kalash Hayaran <kalash123e@gmail.com>
2026-08-17 22:40:15 +0530

    Time: 0 ms (100%), Space: 27.3 MB (68.57%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int idx = 0;
-    unordered_map<int,int> f;
-    TreeNode* fun(vector<int>& pre,int l,int h){
+    // int idx = 0;
+    // unordered_map<int,int> f;
+    TreeNode* fun(vector<int>& pre,int l,int h, unordered_map<int,int> &f, int &idx){
         if(l>h){
             return NULL;
         }
         TreeNode* node = new TreeNode(pre[idx]);
         idx++;
         int id = f[node->val];
-        node->left = fun(pre,l,id-1);
-        node->right = fun(pre,id+1,h);
+        node->left = fun(pre,l,id-1,f,idx);
+        node->right = fun(pre,id+1,h,f,idx);
         return node;
     }
 
     TreeNode* buildTree(vector<int>& pre, vector<int>& in) {
-        // unordered_map<int,int> f;
+        unordered_map<int,int> f;
         for(int i = 0 ; i<in.size(); i++){
             f[in[i]] = i;
         }
         int l = 0;
         int h = in.size()-1;
-        // int idx = 0;
-        return fun(pre,l,h);
+        int idx = 0;
+        return fun(pre,l,h,f,idx);
     }
 };
