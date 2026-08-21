759895e0af2e3388e687c88aa885109a4e77ce17
Kalash Hayaran <kalash123e@gmail.com>
2026-08-22 00:07:10 +0530

    Time: 35 ms (81.75%), Space: 69.4 MB (95.18%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -4,10 +4,9 @@ public:
         sort(arr.begin(),arr.end());
         int count = 0;
         for(int i = 1; i<arr.size(); i++){
-            if(arr[i] <= arr[i-1]){
-                int x = arr[i];
-                arr[i] = arr[i-1] + 1;
-                count = count + arr[i] - x ;
+            if (arr[i] <= arr[i - 1]) {
+                count += arr[i - 1] - arr[i] + 1;
+                arr[i] = arr[i - 1] + 1;
             }
         }
         return count;
