03712800d6b12be6c8fcabec1aab3a259db74e3f
Kalash Hayaran <kalash123e@gmail.com>
2026-09-02 11:52:49 +0530

    Time: 5 ms (43.38%), Space: 39.9 MB (44.21%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string res;
+        for(int i = 0; i<s.size(); i++){
+            if(isalpha(s[i]) ){
+                res.push_back(s[i]);
+            }
+            if(s[i] == '%'){
+                reverse(res.begin(),res.end());
+            }
+            if(s[i] == '*'){
+                if(res != ""){
+                    res.pop_back();
+                }
+                
+            }
+            if(s[i] == '#'){
+                res = res+res;
+            }
+        }
+        return res;
+    }
+};
