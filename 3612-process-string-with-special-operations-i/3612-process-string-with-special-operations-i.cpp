dea7d6f68bea0c0e54b2af3d11a4a424301094f7
Kalash Hayaran <kalash123e@gmail.com>
2026-09-02 11:57:25 +0530

    Time: 5 ms (43.38%), Space: 39.8 MB (72.92%) - LeetHub

--- 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -2,26 +2,21 @@ class Solution {
 public:
     string processStr(string s) {
         string res;
-        for(int i = 0; i<s.size(); i++){
-            if(isalpha(s[i]) ){
-                // res.push_back(s[i]);
-                res = res+s[i];
-                continue;
-            }
-            if(s[i] == '%'){
-                reverse(res.begin(),res.end());
-                continue;
-            }
-            if(s[i] == '*'){
-                if(res != ""){
-                    res.pop_back();
-                    continue;
+        for (int i = 0; i < s.size(); i++) {
+            if (isalpha(s[i])) {
+                res = res + s[i];
+            } else {
+                if (s[i] == '%') {
+                    reverse(res.begin(), res.end());
                 }
-                
-            }
-            if(s[i] == '#'){
-                res = res+res;
-                continue;
+                if (s[i] == '*') {
+                    if (res != "") {
+                        res.pop_back();
+                    }
+                }
+                if (s[i] == '#') {
+                    res = res + res;
+                }
             }
         }
         return res;
