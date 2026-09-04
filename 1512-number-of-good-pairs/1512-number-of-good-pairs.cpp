26d2a411da0f76cec01fe8dd196c14921aad104a
Kalash Hayaran <kalash123e@gmail.com>
2026-09-04 19:31:24 +0530

    Time: 0 ms (100%), Space: 9.8 MB (53.05%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& arr) {
-        int i = 0;
-        int j = i+1;
+        vector<int> f(256,0);
         int count = 0;
-        while(i<arr.size()-1 && j<arr.size()){
-            if(arr[j] == arr[i]){
-                count++;
-            }
-            j++;
-            if(j == arr.size()){
-                i++;
-                j = i+1;
-            }
+        for(int i = 0; i<arr.size(); i++){
+            f[arr[i]]++;
+            count = count + f[arr[i]]-1;
         }
         return count;
     }
