d6b05be97f13291b26b7fe543ae4de16fbbb6674
Kalash Hayaran <kalash123e@gmail.com>
2026-09-05 11:56:49 +0530

    Time: 3 ms (95.01%), Space: 202.5 MB (85.02%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& arr, int k) {
-        vector<int> a(arr.size());
         vector<int> b(arr.size());
         int mn = INT_MAX;
         int mx = INT_MIN;
-        for(int i = 0; i<arr.size(); i++){
-            mx = max(arr[i],mx);
-            a[i] = mx;
-        }
+        // for(int i = 0; i<arr.size(); i++){
+        //     mx = max(arr[i],mx);
+        //     a[i] = mx;
+        // }
         for(int i = arr.size()-1; i>=0; i--){
             mn = min(arr[i],mn);
             b[i] = mn;
         }
         for(int i = 0; i<arr.size(); i++){
-            if(a[i]-b[i] <= k){
+            mx = max(arr[i], mx);
+            if(mx-b[i] <= k){
                 return i;
             }
         }
         return -1;
+        
     }
 };
