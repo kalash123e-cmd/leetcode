9c4ddb6061339528862fd258816f2bdb3b605605
Kalash Hayaran <kalash123e@gmail.com>
2026-09-06 17:41:54 +0530

    Time: 0 ms (100%), Space: 10.3 MB (36.48%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& arr) {
         int n = arr.size();
-        vector<vector<int>> res(n,vector<int>(n));
+        // vector<vector<int>> res(n,vector<int>(n));
         
         for(int i = 0 ; i<n; i++){
             for(int j = i+1; j<n; j++){
